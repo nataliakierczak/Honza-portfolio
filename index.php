43d2e0e0ba74e4c2d128acf748d71451fbb63a7d
Unknown <natalia.kierczak@gmail.com>
2017-04-02 23:15:36 +0200

    2 April

---
+++ index.php
@@ -0,0 +1,264 @@
+<?php
+require 'email.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Jan Vokrouhlecký - účetní</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,500" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Dosis:400,700" rel="stylesheet">
+
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700|Proza+Libre:400,500,800" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar navbar-default navbar-fixed-top topnav">
+        <div class="container topnav">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand topnav" href="#"><i class="fa fa-home fa-2x"></i></a>
+            </div>
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="#sluzby">Služby</a>
+                    </li>
+                    <li>
+                        <a href="#spoluprace">Další spolupráce</a>
+                    </li>
+                    <li>
+                        <a href="#cenik">Ceník</a>
+                    </li>
+                    <li>
+                        <a href="#klienti">Klienti</a>
+                    </li>
+                    <li>
+                        <a href="#kontakt">Kontakt</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="intro-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h1>Jan Vokrouhlecký</h1>
+                        <h3>Účetní</h3>
+                        <hr class="intro-divider">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <section class="sluzby">
+        <a name="sluzby"></a>
+        <div class="content-section-a">
+         <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+<h2 class="text-center">ÚČETNÍ SLUŽBY</h2>
+                    <hr class="intro-divider">
+         </div>
+
+
+        <h3 class="section-heading text-center">Svým klientům poskytuji následující služby:</h3>
+        <div class="col-lg-12">
+
+            <div class="list-group text-center">
+                <a class="list-group-item list-group-item-action">
+                    vedení účetnictví malých podnikatelských subjektů a společenství vlastníků jednotek
+                </a>
+                <a class="list-group-item list-group-item-action">daňová evidence fyzických osob </a>
+                <a class="list-group-item list-group-item-action">kompletní vedení mzdové agendy malých organizací</a>
+                <a class="list-group-item list-group-item-action">vypracování daňových přiznání, hlášení, přehledů a vyúčtování</a>
+        <a class="list-group-item list-group-item-action ">zastupování při jednání s FÚ, ČSSZ, ZP</a>
+        <a class="list-group-item list-group-item-action ">externí zajištění daňového poradenství, odkladu podání daňového přiznání</a>
+
+
+        </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="spoluprace">
+        <a  name="spoluprace"></a>
+        <div class="content-section-b">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12">
+                        <h2 class="text-center">DALŠÍ SPOLUPRÁCE</h2>
+                        <hr class="intro-divider">
+
+            </div>
+
+                <div class="container">
+                    <div class="row">
+                        <h3 class="section-heading text-center">V rámci širší spolupráce někteří moji klienti využívají:</h3>
+                        <div class="col-lg-12">
+                            <div class="list-group text-center">
+                                <a class="list-group-item list-group-item-action">
+                                    integrace účetnictví/daň.evidence se správou nemovitosti, profesionální vyúčtování služeb v bytových domech</a>
+                                <a class="list-group-item list-group-item-action">automatický import bank.pohybů z banky a seznamů faktur a mzdových podkladů z databází a úložišť klienta</a>
+                                <a class="list-group-item list-group-item-action">likvidace a placení faktur dodavatelům(klient autorizuje), fakturace odběratelům</a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+    </section>
+
+
+    <section class="cenik">
+        <a  name="cenik"></a>
+        <div class="content-section-a">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12">
+                    <h2 class="text-center">CENÍK</h2>
+                    <hr class="intro-divider">
+                </div>
+            </div>
+        </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text center">
+                       <p class="text-center">Cena je předmětem individuální dohody dle povahy činnosti klienta, kvality podkladů, (ne)možnosti využití "chytrých" nástrojů a dalších rysů naší spolupráce.<br><br>
+                        K rámcové orientaci: 350 Kč je obvyklá cena za hodinu mé práce. Cena za zaúčtování jednoho účetního případu je obvykle 20 Kč a méně.</p>
+                    </div>
+                </div>
+        </div>
+        </div>
+    </section>
+
+    <section class="klienti">
+        <a  name="klienti"></a>
+    <div class="content-section-b">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-sm-12 text-center">
+                    <h2 class="section-heading">KLIENTI</h2>
+                    <hr class="intro-divider">
+                </div>
+                <div class="row ">
+                <div class="col-md-2 col-md-offset-4 col-xs-12 text-center">
+                    <a href="https://www.titulkovani.cz/index.php?t=info" target="_blank"><img class="img-responsive clients bw" src="img/titulkovani.JPG" alt="Logo"></a>
+                </div>
+                    <div class="col-md-3 col-xs-12 text-center">
+                        <a href="http://www.fema-m.cz/" target="_blank"><img class="img-responsive clients bw" src="img/femam.JPG" alt="Logo"></a>
+                    </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    </section>
+
+
+    <section class="kontakt">
+        <a  name="kontakt"></a>
+        <div class="content-section-a">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-sm-12 text-center">
+                        <h2 class="section-heading">KONTAKT</h2>
+                        <hr class="intro-divider">
+                    </div>
+                </div>
+
+                <div class="container">
+                    <div class="row ">
+                        <div class="col-lg-6">
+                            <h4 class="text-center">Kontaktní formulář</h4>
+                            <form method="post" class="form-horizontal">
+                                <?php if (!empty($msg)) {echo "<h5 class='text-center'>$msg</h5>";} ?>
+                                <div class="form-group">
+                                    <label for="jmeno"  class="col-sm-2 control-label">Jméno a příjmení</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" name="name" class="form-control" id="jmeno" placeholder="Jméno a příjmení">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email"  class="col-sm-2 control-label">Email</label>
+                                    <div class="col-sm-10">
+                                        <input type="email" name="email" class="form-control" id="email" placeholder="Emailová adresa">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="zprava" class="col-sm-2 control-label">Zpráva</label>
+                                    <div class="col-sm-10">
+                                        <textarea class="form-control" name="message" id="zprava" rows="12"  placeholder="Vaše zpráva"></textarea>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-sm-offset-6 col-sm-6">
+                                        <button type="submit" name="send-email" class="btn btn-primary">Odeslat</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+
+                        <div class="col-lg-6 center-block">
+                            <h4 class="text-center">Adresa: Bruselská 11/518 120 00 Praha 2</h4>
+                            <div><div id="map">
+                            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCFiqbrhlauX2VKFDHPhuN4AwES4QKk6pY"></script>
+                            <script async defer src="embed-map.js"></script>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="banner"></div>
+    <section class="modal-footer">
+
+                   <p class="copyright text-muted small text-center">&copy; Jan Vokrouhlecký 2017</p>
+                    <p class="text-muted small text-center">Page by  <a href="http://nataliak.cz/" target="_blank">nataliak</a></p>
+
+
+    </section>
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
